a07a9ce4ce69c0e9ef32d4a961af4be09ebaed69
HuangMaoTongXue <mao_huang@foxmail.com>
2016-09-14 17:00:58 +0800

    finish sax 1.0
    finish the function of sax parse XML

---
+++ saxParseXML/src/saxParseXML/Run.java
@@ -0,0 +1,27 @@
+package saxParseXML;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+
+/**
+ * Created by huangmao on 2016/9/14.
+ */
+public class Run {
+    public static void main(String[] argx){
+        try {
+            SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+            SAXParser parser = parserFactory.newSAXParser();
+            parser.parse("student.xml",new SAXHandler("学生名册"));
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
